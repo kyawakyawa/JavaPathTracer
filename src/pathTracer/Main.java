1f6de9ef4b83be15a4bf819d214ec14e6e434815
kyawakyawa <ikaairplanef15@yahoo.co.jp>
2018-01-22 22:27:00 +0900

    光源

--- src/pathTracer/Main.java
+++ src/pathTracer/Main.java
@@ -19,7 +19,8 @@ public class Main {
 		render.add(new Sphere(new Vecter3(65, 20, 20), 20, new Material(new FColor(b, a, b))));
 		render.add(new Sphere(new Vecter3(27, 16.5, 47), 16.5, new Material(MaterialType.MT_PERFECT_REF)));
 		render.add(new Sphere(new Vecter3(77, 16.5, 78), 16.5, new Material(MaterialType.MT_REFRACTION)));
-		render.addAsLight(new Sphere(new Vecter3(50.0, 73.0, 81.6), 5, new Material(new FColor(0.30, 0.30, 0.30), new FColor(40, 30.901960, 22.431360))));
+		//render.addAsLight(new Sphere(new Vecter3(50.0, 90.0, 81.6), 15, new Material(new FColor(0.30, 0.30, 0.30), new FColor(36, 36, 36))));//純粋パストレ用の光源
+		render.addAsLight(new Sphere(new Vecter3(50.0, 73.0, 81.6), 5, new Material(new FColor(0.30, 0.30, 0.30), new FColor(40, 30.901960, 22.431360))));//NEE用の光源
 
 		render.draw(32,4);//32 * 4 * 4 サンプル
 	}
